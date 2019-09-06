c2e14df74ea9e116b0c66d962d6c60f610aca970
Genkzsz11 <genkzsz11@gmail.com>
2019-09-06 18:17:44 +0700

    Branding DU-REX EXTRASAFE *Also kang Buildinfo.sh from havoc
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,30 +1,20 @@
 # Versioning System
-DU_BASE_VERSION = v13.5
-
-ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := UNOFFICIAL
-endif
-
-# Only include DU-Updater for official, weeklies, and rc builds
-ifeq ($(filter-out OFFICIAL WEEKLIES RC,$(DU_BUILD_TYPE)),)
-    PRODUCT_PACKAGES += \
-        DU-Updater
-endif
-
-# Sign builds if building an official or weekly build
-ifeq ($(filter-out OFFICIAL WEEKLIES,$(DU_BUILD_TYPE)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(KEYS_LOCATION)
-endif
+DU_BASE_VERSION = EXTRASAFE
+DU_BUILD_TYPE := DUREX
 
 # Set all versions
+TARGET_PRODUCT_SHORT := $(subst du_,,$(DU_BUILD_TYPE))
+DU_DATE := $(shell date -u +%d-%m-%Y)
 DATE := $(shell date -u +%Y%m%d)
 TIME := $(shell date -u +%H%M)
-DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(DATE)-$(TIME)-$(DU_BUILD_TYPE)
+DU_BUILD_VERSION := $(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DATE)-$(TIME)
+DU_VERSION := $(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DU_BUILD)-$(DATE)-$(TIME)
 TARGET_BACON_NAME := $(DU_VERSION)
 ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(DU_BUILD_TYPE)/$(DATE)$(TIME)
 
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DATE) \
-    ro.du.fingerprint=$(ROM_FINGERPRINT)
+export DU_BASE_VERSION
+export DU_BUILD_VERSION
+export DU_BUILD_TYPE
+export DU_DATE
+export DU_VERSION
+export ROM_FINGERPRINT

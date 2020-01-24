54226bcf870b0d3319aa6ab88d09e2565529662e
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    vendor: Add Pixel charger animation
    [kubersharma001]: Guard it with a flag, optional for devices who want to opt in.
    Set TARGET_INCLUDE_PIXEL_CHARGER := true in du_codename.mk
    
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Change-Id: Ie0d25e37912fffd337731d4c5eb9e9ccf09a2696

--- config/packages.mk
+++ config/packages.mk
@@ -14,6 +14,13 @@ PRODUCT_PACKAGES += \
     Recorder \
     Seedvault
 
+# Charger mode images
+ifeq ($(TARGET_INCLUDE_PIXEL_CHARGER),true)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+endif
+
 # Allows registering device to Google easier for gapps
 # Integrates package for easier Google Pay fixing
 PRODUCT_PACKAGES += \

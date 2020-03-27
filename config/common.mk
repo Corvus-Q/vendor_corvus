9ea8d924043c61471ba6b6535fe48145cefa1b59
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    [2/4] Port face unlock feature
    Change-Id: I18dde37697ee4e68c9aa133da2fb71c3f03109bd

--- config/common.mk
+++ config/common.mk
@@ -102,4 +102,12 @@ include vendor/corvus/config/packages.mk
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/corvus/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/corvus/overlay/common
 
-$(call inherit-product-if-exists, external/motorola/faceunlock/config.mk)
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)

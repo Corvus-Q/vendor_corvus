44a022283437938313a66ab2974c096217a92f65
Saikiran <bjsaikiran@gmail.com>
2020-08-05 08:32:37 +0530

    Update branding
    - Add codename seperately
    
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Change-Id: I1288e0dd9d96719aff76c9ea56ed1ba2fd56c16f

--- config/branding.mk
+++ config/branding.mk
@@ -1,6 +1,7 @@
 # Set all versions
 DU_BUILD_TYPE ?= UNOFFICIAL
-DU_VERSION_BASE := 8.0-TSUSHIMA
+DU_VERSION_BASE := 8.0
+CRVS_CODENAME := Tsushima
 
 # Set all versions
 BUILD_DATE := $(shell date -u +%d%m%Y)
@@ -9,9 +10,9 @@ BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
 ROM_FINGERPRINT := Corvus/$(DU_BUILD_TYPE)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
 
 ifeq ($(USE_GAPPS), true)
-    DU_VERSION := Corvus_v$(DU_VERSION_BASE)-$(DU_BUILD)-Gapps-$(BUILD_DATE)-$(DU_BUILD_TYPE)-$(BUILD_TIME)
+    DU_VERSION := Corvus_v$(DU_VERSION_BASE)-$(CRVS_CODENAME)-$(DU_BUILD)-Gapps-$(BUILD_DATE)-$(DU_BUILD_TYPE)-$(BUILD_TIME)
 else
-    DU_VERSION := Corvus_v$(DU_VERSION_BASE)-$(DU_BUILD)-$(BUILD_DATE)-$(DU_BUILD_TYPE)-$(BUILD_TIME)
+    DU_VERSION := Corvus_v$(DU_VERSION_BASE)-$(CRVS_CODENAME)-$(DU_BUILD)-$(BUILD_DATE)-$(DU_BUILD_TYPE)-$(BUILD_TIME)
 endif
 
 DU_PROPERTIES := \
@@ -19,6 +20,7 @@ DU_PROPERTIES := \
     ro.build.datetime=$(BUILD_DATE_TIME) \
     ro.du.build.date=$(BUILD_DATE) \
     ro.du.version=$(DU_VERSION) \
+    ro.corvus.codename=$(CRVS_CODENAME) \
     ro.du.build.type=$(DU_BUILD_TYPE) \
     ro.du.build.version=$(DU_VERSION_BASE) \
     ro.du.fingerprint=$(ROM_FINGERPRINT)

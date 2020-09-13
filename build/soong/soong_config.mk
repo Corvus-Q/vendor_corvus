87088557819677ce76344c0a31a41d75239e5cb7
Lucchetto <lucchetto.tie@live.com>
2020-09-13 15:37:24 +0200

    corvus: add soong var for extended compress format
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I6fa7e9e016bc20c348838454de3eda31c0461f57

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -10,6 +10,7 @@ $(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(TAR
 $(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
 $(call add_json_bool, Needs_text_relocations,                $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_bool, Supports_extended_compress_format, $(filter true,$(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)))
 $(call add_json_bool, Supports_hw_fde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Supports_hw_fde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, Should_wait_for_qsee, $(filter true,$(TARGET_KEYMASTER_WAIT_FOR_QSEE)))

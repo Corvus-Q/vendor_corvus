f6023473bcff58621e23613abab0ebb20c42eed7
Ritzz <riteshm321@gmail.com>
2020-09-22 23:56:53 +0200

    vendor: lets be bit formal
    Also update the codename derp
    
    Change-Id: I5ea24d1fb8355b1f0eed1f5c748492e05e30f950

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -42,7 +42,7 @@ corvus: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo -e ${CL_MAG}"...............#######..######..##....##.....###........#######...######............"${CL_MAG}
 	@echo -e ${CL_RST}""${CL_RST}
 	@echo -e ${CL_RST}""${CL_RST}
-	@echo -e ${CL_RST}"                    Build completed! Now flash that shit and ENJOY!                    "${CL_RST}
+	@echo -e ${CL_RST}"                         Build completed! Flash and ENJOY:)                            "${CL_RST}
 	@echo -e ${CL_RST}""${CL_RST}
 	@echo -e ${CL_MAG}"======================================================================================="${CL_MAG}
 	@echo -e ${CL_RST}""Package Complete: "$(INTERNAL_BACON_TARGET)"${CL_RST}
--- config/branding.mk
+++ config/branding.mk
@@ -1,5 +1,5 @@
 # Set all versions
-CORVUS_BUILD_TYPE ?= Unofficial
+RAVEN_LAIR ?= Unofficial
 CORVUS_VERSION_BASE := 9.5-R
 CORVUS_CODENAME := TheStorm
 
@@ -7,12 +7,12 @@ CORVUS_CODENAME := TheStorm
 BUILD_DATE := $(shell date -u +%d%m%Y)
 BUILD_TIME := $(shell date -u +%H%M)
 BUILD_DATE_TIME := $(BUILD_TIME)$(BUILD_DATE)
-ROM_FINGERPRINT := Corvus/$(CORVUS_BUILD_TYPE)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
+ROM_FINGERPRINT := Corvus/$(RAVEN_LAIR)/$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_DATE)/$(BUILD_TIME)
 
 ifeq ($(USE_GAPPS), true)
-    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-Gapps-$(BUILD_DATE)-$(CORVUS_BUILD_TYPE)-$(BUILD_TIME)
+    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-Gapps-$(BUILD_DATE)-$(RAVEN_LAIR)-$(BUILD_TIME)
 else
-    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-$(BUILD_DATE)-$(CORVUS_BUILD_TYPE)-$(BUILD_TIME)
+    CORVUS_VERSION := Corvus_v$(CORVUS_VERSION_BASE)-$(CORVUS_CODENAME)-$(CORVUS_BUILD)-$(BUILD_DATE)-$(RAVEN_LAIR)-$(BUILD_TIME)
 endif
 
 CORVUS_PROPERTIES := \
@@ -20,7 +20,7 @@ CORVUS_PROPERTIES := \
     ro.build.datetime=$(BUILD_DATE_TIME) \
     ro.corvus.build.date=$(BUILD_DATE) \
     ro.corvus.version=$(CORVUS_VERSION) \
-    ro.corvus.codename=$(CRVS_CODENAME) \
-    ro.corvus.build.type=$(CORVUS_BUILD_TYPE) \
+    ro.corvus.codename=$(CORVUS_CODENAME) \
+    ro.corvus.build.type=$(RAVEN_LAIR) \
     ro.corvus.build.version=$(CORVUS_VERSION_BASE) \
     ro.corvus.fingerprint=$(ROM_FINGERPRINT)
